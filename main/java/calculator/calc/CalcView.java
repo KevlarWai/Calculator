282be28a133ac55d1ab7c19dee8b30259bd8aec7
KevlarWai <kevinwai@hotmail.co.uk>
2021-07-04 02:18:41 +0100

    Roughly align buttons in GUI
    roughly align buttons in in GUI

--- main/java/calculator/calc/CalcView.java
+++ main/java/calculator/calc/CalcView.java
@@ -7,10 +7,16 @@ public class CalcView {
 
     private JFrame frame;
 
+    private int width = 300;
+    private int height = 500;
+    private int boarder = 30;
+    private int textx = 20;
+    private int texty = 100;
+
     public CalcView() {
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 500);
+        frame.setSize(width, height);
         JPanel panel = new JPanel();
         JPanel inputArea = new JPanel(); 
         JTextField tf = new JTextField(10);
@@ -28,34 +34,44 @@ public class CalcView {
         JButton subtraction = new JButton("-");
         JButton multiply = new JButton("X");
         JButton divide = new JButton("/");
+
+        button0.setBounds(boarder + (1*((width - 2*boarder)/3)),(texty + (boarder * 2) + (40 * 3)), (width - 2*boarder) / 3, (height-(3*boarder) - texty )/6);
+        button1.setBounds(boarder + (0*((width - 2*boarder)/3)),(texty + (boarder * 2) + (40 * 0)), (width - 2*boarder) / 3, (height-(3*boarder) - texty )/6);
+        button2.setBounds(boarder + (1*((width - 2*boarder)/3)),(texty + (boarder * 2) + (40 * 0)), (width - 2*boarder) / 3, (height-(3*boarder) - texty )/6);
+        button3.setBounds(boarder + (2*((width - 2*boarder)/3)),(texty + (boarder * 2) + (40 * 0)), (width - 2*boarder) / 3, (height-(3*boarder) - texty )/6);
+        button4.setBounds(boarder + (0*((width - 2*boarder)/3)),(texty + (boarder * 2) + (40 * 1)), (width - 2*boarder) / 3, (height-(3*boarder) - texty )/6);
+        button5.setBounds(boarder + (1*((width - 2*boarder)/3)),(texty + (boarder * 2) + (40 * 1)), (width - 2*boarder) / 3, (height-(3*boarder) - texty )/6);
+        button6.setBounds(boarder + (2*((width - 2*boarder)/3)),(texty + (boarder * 2) + (40 * 1)), (width - 2*boarder) / 3, (height-(3*boarder) - texty )/6);
+        button7.setBounds(boarder + (0*((width - 2*boarder)/3)),(texty + (boarder * 2) + (40 * 2)), (width - 2*boarder) / 3, (height-(3*boarder) - texty )/6);
+        button8.setBounds(boarder + (1*((width - 2*boarder)/3)),(texty + (boarder * 2) + (40 * 2)), (width - 2*boarder) / 3, (height-(3*boarder) - texty )/6);
+        button9.setBounds(boarder + (2*((width - 2*boarder)/3)),(texty + (boarder * 2) + (40 * 2)), (width - 2*boarder) / 3, (height-(3*boarder) - texty )/6);
+        addition.setBounds(boarder + (0*((width - 2*boarder)/4)),(texty + (boarder * 2) + (40 * 4)), (width - 2*boarder) / 4, (height-(3*boarder) - texty )/6);
+        subtraction.setBounds(boarder + (1*((width - 2*boarder)/4)),(texty + (boarder * 2) + (40 * 4)), (width - 2*boarder) / 4, (height-(3*boarder) - texty )/6);
+        multiply.setBounds(boarder + (2*((width - 2*boarder)/4)),(texty + (boarder * 2) + (40 * 4)), (width - 2*boarder) / 4, (height-(3*boarder) - texty )/6);
+        divide.setBounds(boarder + (3*((width - 2*boarder)/4)),(texty + (boarder * 2) + (40 * 4)), (width - 2*boarder) / 4, (height-(3*boarder) - texty )/6);
+
+
+
         panel.add(tf);
-        inputArea.add(button1);
-        inputArea.add(button2);
-        inputArea.add(button3);
-        inputArea.add(button4);
-        inputArea.add(button5);
-        inputArea.add(button6);
-        inputArea.add(button7);
-        inputArea.add(button8);
-        inputArea.add(button9);
-        inputArea.add(button0);
-        inputArea.add(addition);
-        inputArea.add(subtraction);
-        inputArea.add(multiply);
-        inputArea.add(divide);
+
+        frame.add(button0);
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(button3);
+        frame.add(button4);
+        frame.add(button5);
+        frame.add(button6);
+        frame.add(button7);
+        frame.add(button8);
+        frame.add(button9);
+        frame.add(addition);
+        frame.add(subtraction);
+        frame.add(multiply);
+        frame.add(divide);
+
 
         frame.getContentPane().add(BorderLayout.NORTH, panel);
         frame.getContentPane().add(BorderLayout.CENTER, inputArea);
-        // frame.getContentPane().add(button1);
-        // frame.getContentPane().add(button2);
-        // frame.getContentPane().add(button3);
-        // frame.getContentPane().add(button4);
-        // frame.getContentPane().add(button5);
-        // frame.getContentPane().add(button6);
-        // frame.getContentPane().add(button7);
-        // frame.getContentPane().add(button8);
-        // frame.getContentPane().add(button9);
-        // frame.getContentPane().add(button0);
         frame.setVisible(true);
     }
 
